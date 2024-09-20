4bfd8dc0f9d34f960cb7f4d840dff2fea75aa1f0
Guilhem Maire <maire.guilhem@gmail.com>
2024-09-20 10:07:14 +0200

    changements dans prixTuring.c

--- PrixTuring/prixTuring.c
+++ PrixTuring/prixTuring.c
@@ -16,15 +16,32 @@
 #include <errno.h>
 #include <assert.h>
 
+#define LIGNE_MAX 600;
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // MAIN
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 typedef struct {
 	int annee;
-	char gagnant[100];
-	char travail[500];
+	char* gagnant;
+	char* travail;
 } gagnantPrixTuring;
+
+void ecrireLigneDansStruct(char* maLigne, gagnantPrixTuring unGagnant){
+        int cpt = 0;
+        char annee[4];
+        while (maLigne[cpt] != ";"){
+                annee[cpt] = maLigne[cpt];
+                cpt++;
+        }
+        cpt++;
+        int compteurDeCaracteresDansLeNom = 0;
+        unGagnant.annee = atoi(annee);
+        char nomDuGagnant[500];
+        while (maLigne[cpt] != ";"){
+                
+        }
+}
 
 int main(int argc, char** argv)
 {
@@ -32,8 +49,23 @@ int main(int argc, char** argv)
 	char outputFilename[] = "out.csv";
 
 	fileCsv = FILE *fopen(filename,"r");
-
-	struct gagnantPrixTuring tableauDeGagnants[1000];
+        char* maLigne = malloc(1024*sizeof(char));
+        gagnantPrixTuring tousLesGagnants[200];
+        
+        for(i=0;;i++){
+                if(feof(filename)){
+                        break;
+                }
+                else {
+                        char* ptrVersMaLigne = fgets(maLigne, 1023, fileCsv);
+                        if(ptrVersMaLigne == NULL){
+                                break;
+                        }
+                        else{
+                                ecrireLigneDansStruct(maLigne,tousLesGagnants[i]);
+                        }
+                }
+        }
 	
 
 
